334d9e720a47c0911a37bb7d921c5263281e5471
Dragonclaw101 <Huppjoesaphean@gmail.com>
2021-02-11 08:58:33 -0800

    worked with functions in class. (02/11/2020)

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,25 @@
-'use strict';
+//Javascript Functions
 
-//Hello world for proof of life
-console.log('Hello World');
+//Declare the function
+//Name the function
+//Define the function (What does it do?)
+//Call it
+
+function add(a, b){ //Passengers are called parameters
+
+//One way: return a+b;
+
+//Another way:
+
+//var vs let - different (scoping). Let is the proper way to define a variable.
+let sum = a + b;
+return sum;
+
+}
+
+let a = 1;
+let b = 5;
+
+//console.log(a+b);
+console.log(add(a,b)); //Don't have to use pre-defined variables, can add what you want in numbers.
+console.log('Hello!');
