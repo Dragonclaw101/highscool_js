7af25a146d70cbf92df5cd002b29c13223b691df
Dragonclaw101 <Huppjoesaphean@gmail.com>
2021-02-11 08:44:34 -0800

    Added variables and console commands for proof of life.

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,22 @@
 
 //Hello world for proof of life
 console.log('Hello World');
+
+//What are variables
+//Something you can assign a value to.
+
+var myVar = 5;
+
+console.log('myVar is ', myVar);
+
+//Math! + printing to consol
+
+console.log('myVar is ', myVar + 3);
+
+//Strings? How do we handle them?
+
+var myString = 'is really cool.';
+var myAddOnn = 'This is the best language in the world.';
+console.log('JavaScript ', myString + " " + myAddOnn, myVar + 3); //Can also do +" "+ to add space.
+
+//Adding strings is called "concatenation." We concatenate strings together. Short: Concat.
